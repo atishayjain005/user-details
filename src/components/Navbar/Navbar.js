adb53e618ca850ac73b8157e9023bf163f6b62cd
Atishay Jain <atishay005@gmail.com>
2022-08-12 11:00:02 +0530

    completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar({button}) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid px-5">
-        <a className="navbar-brand mb-0 h1" href="#1">
+        <a className="navbar-brand mb-0 h1" href="/">
           User Details
         </a>
         {button}
